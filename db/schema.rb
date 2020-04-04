1a6e91334470436dafcdc99004335b27e5d1ef07
Aabid Shamji <aabidshamji@gmail.com>
2020-04-03 19:37:45 -0500

    updated form for submitting the item for a trade

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_235541) do
+ActiveRecord::Schema.define(version: 2020_04_04_000912) do
+
+  create_table "trades", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_trades_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_04_03_235541) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "trades", "users"
 end
